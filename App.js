52ceb96adbf560777fa6a02c204b3ed3f4fbd8a1
Marianny Fidelis <mariannyfidelis@gmail.com>
2021-07-20 13:39:34 -0300

    Trabalhando estaticamente com props

--- App.js
+++ App.js
@@ -1,30 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
-export default function App() {
+class App extends Component {
+  render() {
+    let nome = "Marianny";
 
-  let nome="Mariana";
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+      },
+    });
 
-  return (
-    <View style={styles.container}>
-      <Text style={{fontSize:25, color:`#FF00AA`}}>Primeiro Projeto no React {nome}</Text>
-      <Text style={{fontSize:30, color: `#00FFFF`}}>Marianny</Text>
-      <StatusBar style="auto" />
+    return (
+      <View style={styles.container}>
+        <Text style={{ fontSize: 25, color: `#FF00AA` }}>
+          Primeiro Projeto no React
+        </Text>
+        <Text style={{ fontSize: 30, color: `#00FFFF` }}>Marianny</Text>
+        <Text style={{ fontSize: 25, color: `#FF00AA` }}>
+          Criado por: {nome}
+        </Text>
+        <Image
+          source={{
+            uri: "https://i.pinimg.com/originals/e2/47/28/e24728de466d0786868494ac94d61aa5.jpg",
+          }}
+          style={{ height: 300, width: 300 }}
+        />
 
-      <Image 
-        source={{uri:"https://i.pinimg.com/originals/e2/47/28/e24728de466d0786868494ac94d61aa5.jpg"}}
-        style={{height:300, width:300}}
-      />
-    </View>
-  );
+        <Jobs largura={200} altura={200} />
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+//Criando um componente por meio de classes
+class Jobs extends Component {
+  render() {
+    let img =
+      "https://i.pinimg.com/originals/e2/47/28/e24728de466d0786868494ac94d61aa5.jpg";
+    return(
+    <View>
+      <Image
+        source={{ uri: img }}
+        style={{ width: this.props.largura, height: this.props.altura }}
+      />
+      <Text>Componente imagem !</Text>
+    </View>);
+  }
+}
